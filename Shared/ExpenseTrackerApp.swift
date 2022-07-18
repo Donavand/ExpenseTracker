e409b149b036c03d6cc6014e52f3c7933d1fef40
donavand <danielsdk99@hotmail.com>
2022-07-18 10:08:19 -0400

    Initial Commit

---
+++ Shared/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  Shared
+//
+//  Created by Donavan Daniels on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
